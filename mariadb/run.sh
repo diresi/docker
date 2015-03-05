30205ff5604509f075762bdedf4abbd9da8d7ab2
Christoph Rissner <resi@rissner.net>
2015-03-05 13:28:43 +0100

    mariadb: fixed user creation

--- mariadb/run.sh
+++ mariadb/run.sh
@@ -3,8 +3,6 @@ set -e
 mysqld_safe &
 mysqladmin --silent --wait=30 ping || exit 1
 mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"localhost" WITH GRANT OPTION;'
-mysql -e 'CREATE USER "flinkwork"@"%" IDENTIFIED BY "flinkwork";'
-mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "flinkwork"@"%";'
-mysql -e 'CREATE USER "phabricator"@"%" IDENTIFIED BY "phabricator";'
-mysql -e 'GRANT ALL PRIVILEGES ON `phabricator\_%`.* TO "phabricator"@"%";'
+mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "flinkwork"@"%" IDENTIFIED BY "flinkwork";'
+mysql -e 'GRANT ALL PRIVILEGES ON `phabricator\_%`.* TO "phabricator"@"%" IDENTIFIED BY "phabricator";'
 wait
