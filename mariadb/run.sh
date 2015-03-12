3991090ce44fb3cbf40e6ff05615e967162e0abe
Christoph Rissner <resi@rissner.net>
2015-03-12 09:40:11 +0100

    mariadb: run mysqlcheck during startup, docker shutdowns appearently aren't graceful

--- mariadb/run.sh
+++ mariadb/run.sh
@@ -2,6 +2,7 @@ set -e
 
 mysqld_safe &
 mysqladmin --silent --wait=30 ping || exit 1
+mysqlcheck --all-databases
 mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "root"@"localhost" WITH GRANT OPTION;'
 mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "flinkwork"@"%" IDENTIFIED BY "flinkwork";'
 mysql -e 'GRANT ALL PRIVILEGES ON `phabricator\_%`.* TO "phabricator"@"%" IDENTIFIED BY "phabricator";'
